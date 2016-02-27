34034d9e53f062e6ee6c8831660163c3a12971a9
Taylor Peterson <TaylorP913@Gmail.com>
2016-02-27 10:31:04 -0700

    Added Keil files for Testing

--- Keil Files/Echo.c
+++ Keil Files/Echo.c
@@ -1,9 +1,12 @@
 #include "TM4C123GH6PM.h"
 
-int echo ()
-{
-	char buffer [32];
-	gets ( buffer );
-	printf ( buffer );
-	return 1;
+
+void main(void){
+	int echo ()
+	{
+		char buffer [32];
+		gets ( buffer );
+		printf ( buffer );
+		return 1;
+	}
 }
---
+++ Keil Files/Testing Buffer Overflow/TM4C123GH6PM_C/main.c
@@ -0,0 +1,9 @@
+int main(void)
+{
+	char buffer[5];
+	
+	while(1){
+		
+		
+	}
+}
